52a4321e778da59b9abef24fdf5cec5090cc8024
Chandler Klüser <chandler.kluser@gmail.com>
2018-11-15 16:33:25 -0200

    Sistema de Avisos funcionando sem problemas, migrations na tabela User foram necessárias, o responsável pelo Model deve certificar-se de que os campos criados sejam obrigatórios no registro e na edição

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_012610) do
+ActiveRecord::Schema.define(version: 2018_11_15_181247) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_012610) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "funcao"
+    t.integer "apartamento"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
