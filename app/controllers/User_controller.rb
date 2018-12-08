b98ddcd80306db1363637d85bbaba66e4099b421
Nattypf <45720072+Nattypf@users.noreply.github.com>
2018-12-08 19:35:07 -0200

    Update User_controller.rb

--- app/controllers/User_controller.rb
+++ app/controllers/User_controller.rb
@@ -15,12 +15,12 @@ class UsersController < ApplicationController
   # Use callbacks to share common setup or constraints between
     actions.
   def set_user
-    @user = User.nd(params[:id])
+    @user = User.find(params[:id])
   end
   # Never trust parameters from the scary internet, only allow the
     white list through.
   def user_params
     params.require(:user).permit(:email, :password, :
-    password_conrmation)
+    password_confirmation)
   end
 end
