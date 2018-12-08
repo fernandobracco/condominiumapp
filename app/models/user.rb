839a2ae09dd33b26dde83b79ad34c5ef93a4f297
Nattypf <45720072+Nattypf@users.noreply.github.com>
2018-12-08 19:17:30 -0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  has_many :conversations
+  has_secure_password
 end
