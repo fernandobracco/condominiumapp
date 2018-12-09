e57a069f130247b1ffe535a2db4749b8afe523a9
jrafael21 <43681632+jrafael21@users.noreply.github.com>
2018-12-09 19:02:37 -0200

    Add files via upload

---
+++ config/config/routes.rb
@@ -0,0 +1,27 @@
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+    get 'inicio' => 'avisos#suatela'
+    resources :avisos do
+        member do
+            post:criar_aviso
+        end
+    end
+    resources :pesquisa_avisos
+    resources :encomendas
+    resources :conversations do
+    resources :messages
+  end
+
+  devise_for :users
+  root to: 'conversations#index'
+
+  root to: 'discussions#index'
+  resources :discussions
+  #route para discussão, falar com zé Olenscki caso tenha duvida
+
+  root to: 'calendarios#index'
+  resources :calendarios
+  #route para criação eventos, falar com zé Rafael caso tenha duvida
+
+end
