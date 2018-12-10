b969d6f97d95d6473f1e556313b8b3df1d6317c5
fernandobracco <45015522+fernandobracco@users.noreply.github.com>
2018-12-09 22:46:29 -0200

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,23 +2,16 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Condominium
   class Application < Rails::Application
     config.active_job.queue_adapter = :sidekiq
     config.application_name = 'Condominium App'
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.available_locales = [:en, :'pt-BR']
     config.i18n.default_locale = :'pt-BR'
     config.active_record.default_timezone = :local
     config.load_defaults 5.1
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
